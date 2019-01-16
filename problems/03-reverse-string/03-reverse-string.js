504dfa6833ccec3c431b5ae1f9e5e2c9706b8245
Mileena <mgarc117@ucsc.edu>
2019-01-16 18:36:26 -0500

    3 done!

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,9 +1,25 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
+
+
+//A reverse string function writes the elements in a string backwards
+
 function reverseStr(someStr) {
-  
+    let splitString = someStr.split(""); // ["h", "e", "l", "l", "o"] makes a new array
+    let reverseArray = splitString.reverse(); // ["o", "l", "l", "e", "h"] reverses array
+    let joinArray = reverseArray.join(""); // "olleh" joins all elements to make a single string
+    return joinArray;
 }
+ 
+result = reverseStr("hello");
+console.log(result)
+//output: olleh
 
 
-//test for steph
+//we can also chain all three methods together to keep it consice
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+name = reverseString("Mileena");
+console.log(name)
