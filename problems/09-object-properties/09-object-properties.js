a38417da961facc495b3e8f065e3734a0637650d
Mileena <mgarc117@ucsc.edu>
2019-01-18 19:37:30 -0500

    Done!

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,20 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
-
+function objectProperties(someObj) { //Don't want to create an object we want it to print all of the properties of an object
+	let text = '';   //you can't add an undefined variable to a string
+	let i;
+	for (i in someObj) {               //loops through properties in an object
+		text += someObj[i] + '';
+		//console.log(someObj) prints 3X3
+	};
+	return (console.log(someObj));
 }
+
+let bike = {
+	frontGears: 3,
+	backGears: 7,
+	material: 'aluminum'
+};
+
+result = objectProperties(bike);
